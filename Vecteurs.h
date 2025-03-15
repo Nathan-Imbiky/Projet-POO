3a21e0df183d2ec049b572358447027331ad2ba1
Nathan-Imbiky <nimbiky@gmail.com>
2025-03-15 16:37:28 +0100

    Fix accents vecteurs.h

--- Vecteurs.h
+++ Vecteurs.h
@@ -5,29 +5,29 @@
 class Vecteur
 {
 	private :
-	std::vector<double> coords; //coordonn‚es du vecteur
-	double norme2() const; //calcule le carr‚ de la norme du vecteur
+	std::vector<double> coords; //coordonnees du vecteur
+	double norme2() const; //calcule le carre de la norme du vecteur
 	
 	
 	public :
-	std::vector<double> get_coords() const; //Retourne les coordonn‚es du vecteur
-	void augmente(double x=0); //augmente la dimension du vecteur et ajoute une valeur dans la "case" ajout‚e (valeur 0 par d‚faut par pr‚caution)
-	void set_coord(std::size_t i, double x); //permet de modifier une coordonn‚e du vecteur (on entre la valeur x … l'indice i)
+	std::vector<double> get_coords() const; //Retourne les coordonnees du vecteur
+	void augmente(double x=0); //augmente la dimension du vecteur et ajoute une valeur dans la "case" ajoutee (valeur 0 par defaut par precaution)
+	void set_coord(std::size_t i, double x); //permet de modifier une coordonnae du vecteur (on entre la valeur x a l'indice i)
 	
 	double norme() const; // calcule la norme
 	
 	//produits :
-	double prod_scal(Vecteur autre) const; // produit scalaire de vecteurs; si les dimensions ne sont pas ‚gales on lan‡e une exception 
-	Vecteur prod_vect(Vecteur autre) const; // produit vectoriel (d‚fini uniquement pour la dimension 3; autrement on lance une exception)
+	double prod_scal(Vecteur autre) const; // produit scalaire de vecteurs; si les dimensions ne sont pas egales on lance une exception 
+	Vecteur prod_vect(Vecteur autre) const; // produit vectoriel (defini uniquement pour la dimension 3; autrement on lance une exception)
 	
 	//constructeurs :
-	explicit Vecteur(unsigned int n); // ajoute n dimensions au vecteur et initialise chaque coordonn‚e … 0
+	explicit Vecteur(unsigned int n); // ajoute n dimensions au vecteur et initialise chaque coordonnee a 0
 	Vecteur(double x, double y, double z); //construit le vecteur 3D (x, y, z)
-	Vecteur(std::list<double> L); //construit le vecteur … partir d'une liste 
+	Vecteur(std::list<double> L); //construit le vecteur a partir d'une liste; pour cause d'ambiguite, preferer l'utilisation du constructeur de la ligne precedente pour 3 dimensions
 	
-	//op‚rateurs internes :
-	bool operator==(Vecteur const& autre); //d‚termine si notre vecteur est ‚gal … "autre", remplace la fonction compare()
-	bool operator!=(Vecteur const& autre); //simplement "non v==autre", o— v est notre vecteur
+	//operateurs internes :
+	bool operator==(Vecteur const& autre); //determine si notre vecteur est egal a "autre", remplace la fonction compare()
+	bool operator!=(Vecteur const& autre); //simplement "non v==autre", ou v est notre vecteur
 	
 	Vecteur& operator+=(Vecteur const& autre); //additionne au notre le vecteur autre
 	Vecteur& operator-=(Vecteur const& autre); //soustrait au notre le vecteur autre
@@ -37,18 +37,18 @@ class Vecteur
 };
 
 
-//op‚rateurs internes :
+//operateurs internes :
 std::ostream& operator<<(std::ostream& out, Vecteur const& v); //affiche v (cout<<v)
 
-Vecteur operator^(Vecteur v1, Vecteur const& v2); //retourne le produit vectoriel de v1 avec v2 (v1^v2)
-double operator*(Vecteur const&v1, Vecteur const& v2); //effectue le produit scalaire de v1 et v2
-Vecteur operator-(Vecteur v); //retourne -v l'oppos‚ de v
+Vecteur operator^(Vecteur v1, Vecteur const& v2); //retourne le produit vectoriel de v1 avec v2 (v1^v2); en cas de probleme, retourne le vecteur 3d nul avec un warning
+double operator*(Vecteur const&v1, Vecteur const& v2); //effectue le produit scalaire de v1 et v2; en cas de probleme, retourne 0 avec un warning
+Vecteur operator-(Vecteur v); //retourne -v l'oppose de v
 Vecteur operator~(Vecteur const& v); //retourne le vecteur unitaire dans le sens et la direction de v
 
 Vecteur operator*(Vecteur v, double const& a); 
-Vecteur operator*(double const& a, Vecteur v); //multiplication scalaire des deux c“t‚s
+Vecteur operator*(double const& a, Vecteur v); //multiplication scalaire des deux cotes
 Vecteur operator/(Vecteur v, double const& a);
-Vecteur operator/(double const& a, Vecteur v); //division scalaire des deux c“t‚s
+Vecteur operator/(double const& a, Vecteur v); //division scalaire des deux cotes
 
 Vecteur operator+(Vecteur v1, Vecteur const& v2); // addition de vecteurs
 Vecteur operator-(Vecteur v1, Vecteur const& v2); // soustraction de vecteurs
